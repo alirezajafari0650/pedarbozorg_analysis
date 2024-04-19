6446d7bba654b6c0e58ca02acdddb0800e104f16
jafar <jafar0650developer@gmail.com>
2024-04-19 13:11:10 +0330

    admin added

--- jafar_analyst/admin.py
+++ jafar_analyst/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Customer, Product, SubOrder, Order
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(SubOrder)
+admin.site.register(Order)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ['phone', 'name']
+
+    # when a customer delete , delete all orders with empty suborders
+    def delete_model(self, request, obj):
+        orders = Order.objects.all()
+        for order in orders:
+            if not order.sub_orders.all():
+                order.delete()
+        obj.delete()
+
+
+admin.site.register(Customer, CustomerAdmin)
